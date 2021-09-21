9911cddd33e1a3f59b3169dba074c659cc51b7cf
André Bedregal <andreb@iptp.pe>
2021-09-20 22:37:25 -0500

    upload files with axios

--- app/features/chatbox/chatboxAttach.js
+++ app/features/chatbox/chatboxAttach.js
@@ -1,17 +1,19 @@
 define([
+    'axios',
     'app/constant',
     'shared/data',
     'shared/alert',
     'shared/functions',
     'features/modal/modalPhoneRequest'
 ], (
+    axios,
     constant,
     GLOBAL,
     ALERT,
     functions,
     modalPhoneRequestComp
 ) => {
-    const { API_URL, ACCESS_TOKEN } = constant;
+    const { API_URL } = constant;
     let $attachButton;
     let $inputFile;
     let $inputImage;
@@ -84,33 +86,21 @@ define([
     };
 
     const callAPI = (endpoint, file) => {
-        const fd = new FormData();
+        const formData = new window.FormData();
         $progressWrapper.show();
-        fd.append('file', file);
-        fd.append('chat_id', GLOBAL.getCurrentRoomId());
+        formData.append('file', file);
+        formData.append('chat_id', GLOBAL.getCurrentRoomId());
 
-        $.ajax({
-            type: 'POST',
-            url: `${API_URL}/${endpoint}`,
-            data: fd,
-            headers: {
-                Authorization: `Bearer ${(functions.getDataToLocalApplication(ACCESS_TOKEN) || '')}`
-            },
-            xhr() {
-                const myXhr = $.ajaxSettings.xhr();
-
-                if (myXhr.upload) {
-                    myXhr.upload.addEventListener('progress', progressUpload, false);
-                }
-
-                return myXhr;
-            },
-            cache: false,
-            contentType: false,
-            processData: false,
-            success: hideProcess,
-            error: hideProcess
-        });
+        axios.post(`${API_URL}/${endpoint}`,
+            formData,
+            {
+                headers: {
+                    'Content-Type': 'multipart/form-data'
+                },
+                onUploadProgress: (progressEvent) => progressUpload(progressEvent)
+            })
+            .then(() => hideProcess())
+            .catch(() => hideProcess());
     };
 
     const checkFile = (file, isMedia) => {
@@ -145,7 +135,6 @@ define([
 
     const uploadFile = (endpoint) => {
         if (endpoint === 'fileupload') {
-            console.log($inputFile.get(0).files);
             callAPI(endpoint, $inputFile.get(0).files[0]);
         } else {
             checkFile($inputImage.get(0).files[0], true);
@@ -175,8 +164,6 @@ define([
                 onOk: () => checkFile(file)
             });
         }
-
-        return false
     };
 
     const showPhoneModal = () => {
@@ -202,11 +189,11 @@ define([
             $callBtn.off().click(showPhoneModal);
             $inputFile.off().change(() => {
                 offEventClickOutside();
-                uploadFile('fileupload')
+                uploadFile('fileupload');
             });
             $inputImage.off().change(() => {
                 offEventClickOutside();
-                uploadFile('imageupload')
+                uploadFile('imageupload');
             });
             $dropzone.off().on('dragover', false).on('drop', onDrop);
 
@@ -226,19 +213,19 @@ define([
                         $dropzone.hide();
                     }
                 }, false);
-                document.addEventListener('drop', e => {
+                document.addEventListener('drop', () => {
                     counter = 0;
                     $dropzone.hide();
                 });
-                document.onpaste = (e) => {
-                    const items = (event.clipboardData || event.originalEvent.clipboardData).items;
-                    for (index in items) {
-                        const item = items[index];
+                document.onpaste = (event) => {
+                    const { items } = (event.clipboardData || event.originalEvent.clipboardData);
+                    Object.keys(items).forEach((key) => {
+                        const item = items[key];
                         if (item.kind === 'file' && GLOBAL.getCurrentRoomId()) {
                             const file = item.getAsFile();
                             checkFile(file);
                         }
-                    }
+                    });
                 };
             }
         },
